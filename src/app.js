9a8f43dcede42e24b8d671f5a548b817eb3780f1
Masood_Alam <mohamed.masood.alam@philips.com>
2020-02-09 21:41:44 +0530

    Update app.js to jsx

--- src/app.js
+++ src/app.js
@@ -1,14 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Pet from './Pet';
+import React from "react";
+import ReactDOM from "react-dom";
+import Pet from "./Pet";
 const App = () => {
-
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, "Adopt Me!"),
-    <Pet name="Pepper" animal="Dog" breed="Labs"/>,
-    <Pet name="Jingles" animal="Bird" breed="Parrot"/>,
-    <Pet name="Salt" animal="Cat" breed="Mix"/>
-  ]);
+  // return React.createElement("div", {}, [
+  //   React.createElement("h1", {}, "Adopt Me!"),
+  //   <Pet name="Pepper" animal="Dog" breed="Labs"/>,
+  //   <Pet name="Jingles" animal="Bird" breed="Parrot"/>,
+  //   <Pet name="Salt" animal="Cat" breed="Mix"/>
+  // ]);
+  return (
+    <div>
+      <h1>Adopt Me!</h1>
+      <Pet name="Pepper" animal="Dog" breed="Labs" />,
+      <Pet name="Jingles" animal="Bird" breed="Parrot" />,
+      <Pet name="Salt" animal="Cat" breed="Mix" />
+    </div>
+  );
 };
 
 ReactDOM.render(React.createElement(App), document.getElementById("root"));
